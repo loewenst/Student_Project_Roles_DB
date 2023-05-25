e4a763a4feb96938c78e19cbcdfd90bd619562b7
Daniel Loewenstein <loewenst@gmail.com>
2023-05-25 17:05:53 -0400

    achieved authentication woooooo

---
+++ student-project-roles-app/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var router = express.Router()
+const passport = require('passport')
+const classCtrl = require('../controllers/classes')
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index')
+})
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  })
+)
+router.get('/logout', (req, res) => {
+  req.logout(() => {
+    res.redirect('/')
+  })
+})
+router.get('/classes/', classCtrl.index)
+module.exports = router
