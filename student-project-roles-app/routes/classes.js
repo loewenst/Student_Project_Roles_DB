f5feef8f4cd06a3944838c6f235596cd866f9b87
Daniel Loewenstein <loewenst@gmail.com>
2023-05-30 15:18:11 -0400

    added ensureLoggedIn functionality

--- student-project-roles-app/routes/classes.js
+++ student-project-roles-app/routes/classes.js
@@ -2,9 +2,10 @@ var express = require('express')
 var router = express.Router()
 const passport = require('passport')
 const classCtrl = require('../controllers/classes')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
-router.get('/', classCtrl.index)
-router.get('/new', classCtrl.new)
-router.post('/', classCtrl.create)
+router.get('/', ensureLoggedIn, classCtrl.index)
+router.get('/new', ensureLoggedIn, classCtrl.new)
+router.post('/', ensureLoggedIn, classCtrl.create)
 
 module.exports = router
